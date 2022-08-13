b291e38e07d806984184aa297df196d44dc1424b
Dell <Dell@DESKTOP-ULMMDGA>
2022-08-13 14:25:47 +0530

    first commit

---
+++ saturday/src/main/java/saturday/sample.java
@@ -0,0 +1,17 @@
+package saturday;
+
+public class sample {
+
+	public void A() {
+		System.out.println("adhi");
+	}
+	
+	public void B() {
+		System.out.println("sakthi");
+	}
+	
+	public void C() {
+		System.out.println("kdsfjlk");
+	}
+	
+}
